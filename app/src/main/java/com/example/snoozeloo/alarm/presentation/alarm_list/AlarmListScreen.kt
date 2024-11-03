20635c7670abc72fb1dd715275da73b4a3d2f28c
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-11-03 14:14:49 +0100

    Added alarm list (no alarms) screen

---
+++ app/src/main/java/com/example/snoozeloo/alarm/presentation/alarm_list/AlarmListScreen.kt
@@ -0,0 +1,96 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.snoozeloo.R
+
+@Composable
+fun AlarmListScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 80.dp)
+    ){
+        Scaffold (
+            topBar = {
+                Title()
+            },
+            floatingActionButton = {
+                CreateAlarmButton()
+            },
+            floatingActionButtonPosition = FabPosition.Center
+        ) { paddingValues ->
+            Column (
+                modifier = modifier
+                    .padding(paddingValues)
+            ) {
+                NoAlarmsFound()
+            }
+        }
+    }
+}
+
+@Composable
+private fun Title() {
+    Text(
+        text = stringResource(R.string.alarms_list_title),
+        style = TextStyle(fontSize = 28.sp)
+    )
+}
+
+@Composable
+private fun CreateAlarmButton() {
+    FloatingActionButton(
+        modifier = Modifier.size(65.dp),
+        onClick = {},
+        shape = RoundedCornerShape((50.dp)),
+        containerColor = colorResource(R.color.primary_color)
+    ) {
+        Icon(
+            modifier = Modifier.size(40.dp),
+            imageVector = Icons.Filled.Add,
+            contentDescription = "Add",
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+private fun NoAlarmsFound() {
+    Column (
+        modifier = Modifier.fillMaxSize().padding(bottom = 80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            modifier = Modifier.padding(30.dp).size(70.dp),
+            painter = painterResource(id = R.drawable.alarm),
+            contentDescription = "",
+            tint = colorResource(R.color.primary_color)
+        )
+        Text(
+            text = stringResource(R.string.no_alarms),
+            textAlign = TextAlign.Center
+        )
+    }
+}
