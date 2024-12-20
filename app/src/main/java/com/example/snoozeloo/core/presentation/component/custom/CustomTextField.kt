4b27c23d34ed6927ce2c32c93344aaae2a4e4d11
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-12-20 18:16:50 +0100

    Added CustomTextField composable

---
+++ app/src/main/java/com/example/snoozeloo/core/presentation/component/custom/CustomTextField.kt
@@ -0,0 +1,54 @@
+package com.example.snoozeloo.core.presentation.component.custom
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CustomTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (newValue: String) -> Unit
+) {
+    BasicTextField(
+        modifier = modifier
+            .height(37.dp)
+            .width(296.dp),
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        maxLines = 1,
+        textStyle = TextStyle(
+            fontSize = 14.sp,
+            fontStyle = FontStyle.Normal
+        ),
+        decorationBox = { innerTextField ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(5.dp)),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                ) {
+                    innerTextField()
+                }
+            }
+        }
+    )
+}
