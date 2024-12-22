27436440147a8de6c3afd34b82bd06591a21f223
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-12-22 22:24:37 +0100

    Connected Koin and db

--- app/src/main/java/com/example/snoozeloo/core/di/AppModule.kt
+++ app/src/main/java/com/example/snoozeloo/core/di/AppModule.kt
@@ -1,9 +1,11 @@
 package com.example.snoozeloo.core.di
 
+import com.example.snoozeloo.MainApplication
 import com.example.snoozeloo.core.data.AlarmRepository
 import com.example.snoozeloo.core.data.AlarmRepositoryImpl
 import com.example.snoozeloo.core.presentation.alarm_list.AlarmListViewModel
 import com.example.snoozeloo.core.presentation.alarm_create.AlarmCreateViewModel
+import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
@@ -11,6 +13,10 @@ import org.koin.dsl.module
 
 
 val appModule = module {
+    single {
+        (androidApplication() as MainApplication).database.dao
+    }
+
     singleOf(::AlarmRepositoryImpl) { bind<AlarmRepository>() }
 
     viewModelOf(::AlarmListViewModel)
