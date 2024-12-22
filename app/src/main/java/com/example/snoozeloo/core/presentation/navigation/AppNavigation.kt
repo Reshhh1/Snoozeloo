ba1e0132328cd64136c54ac73bd2053d2897c6c5
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-12-22 22:25:12 +0100

    Added necessary params

--- app/src/main/java/com/example/snoozeloo/core/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/snoozeloo/core/presentation/navigation/AppNavigation.kt
@@ -2,12 +2,16 @@ package com.example.snoozeloo.core.presentation.navigation
 
 import AlarmListScreen
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.snoozeloo.core.presentation.alarm_create.AlarmCreateScreen
+import com.example.snoozeloo.core.presentation.alarm_create.AlarmCreateViewModel
 import com.example.snoozeloo.core.presentation.navigation.destination.AlarmCreate
 import com.example.snoozeloo.core.presentation.navigation.destination.AlarmList
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun AppNavigation() {
@@ -22,8 +26,12 @@ fun AppNavigation() {
             )
         }
         composable<AlarmCreate> {
+            val viewModel = koinViewModel<AlarmCreateViewModel>()
+            val state by viewModel.state.collectAsState()
             AlarmCreateScreen(
-                navigateToList = { navController.navigate(AlarmList) }
+                navigateToList = { navController.navigate(AlarmList) },
+                onEvent = viewModel::onEvent,
+                state = state
             )
         }
     }
