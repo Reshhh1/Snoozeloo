38b61a6b4445728b10a9c7ac93816445a1adf10f
Reshhh1 <reshwanbarhoe1@gmail.com>
2025-01-03 18:46:32 +0100

    Implemented AlarmTime logic

--- app/src/main/java/com/example/snoozeloo/core/presentation/alarm_create/AlarmTime.kt
+++ app/src/main/java/com/example/snoozeloo/core/presentation/alarm_create/AlarmTime.kt
@@ -1,6 +1,37 @@
 package com.example.snoozeloo.core.presentation.alarm_create
 
+import androidx.core.text.isDigitsOnly
+
 data class AlarmTime(
-    val initialHour: String,
-    val initialMinute: String
-)
+    var initialHour: String,
+    var initialMinute: String
+) {
+
+    fun validateTimeHours(): Boolean {
+        return validateTimeDigit(initialHour)
+    }
+
+    fun validateTimeMinutes(): Boolean {
+        return validateTimeDigit(initialMinute)
+    }
+
+    /**
+     * Normalize time to double digit
+     * @param timeToNormalize time to normalize
+     * @param maxTimeValue max time value
+     */
+    fun timeNormalizer(timeToNormalize: String, maxTimeValue: Int): String {
+        val time = timeToNormalize.toIntOrNull() ?: return ""
+        val maxDoubleDigit = maxTimeValue.toString().first().digitToInt()
+        return when {
+            timeToNormalize.length == 2 && time > maxTimeValue -> "00"
+            timeToNormalize.length == 1 && time > maxDoubleDigit -> "0$timeToNormalize"
+            else -> timeToNormalize
+        }
+    }
+
+
+    private fun validateTimeDigit(toBeValidated: String): Boolean {
+        return toBeValidated.length in 0..2 && toBeValidated.isDigitsOnly()
+    }
+}
